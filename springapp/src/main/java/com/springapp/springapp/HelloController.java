60dc10b676ccd9218263b936d52f14e236f824d9
suryaAmmisetti <pavidipallas@gmail.com>
2022-11-08 14:59:13 +0530

    login-reg-navigation

--- springapp/src/main/java/com/springapp/springapp/HelloController.java
+++ springapp/src/main/java/com/springapp/springapp/HelloController.java
@@ -1,12 +1,19 @@
 package com.springapp.springapp;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-    @RequestMapping("")
-    public String hello() {
-        return "Surya - Nitish - Application";
+
+    @RequestMapping()
+    String getHomePage(Model model) {
+        return "index";
+    }
+
+    @RequestMapping(value = "/register")
+    String getRegistrationPage() {
+        return "register";
     }
 }
